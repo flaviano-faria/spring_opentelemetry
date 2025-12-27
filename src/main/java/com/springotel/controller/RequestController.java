34b7a44a9bb72a10da6bd70222627d6159795a34
flaviano-faria <flsilv89@yahoo.com.br>
2025-12-27 15:52:00 -0300

    Updating config files

--- src/main/java/com/springotel/controller/RequestController.java
+++ src/main/java/com/springotel/controller/RequestController.java
@@ -1,7 +1,7 @@
 package com.springotel.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
 
-    private static final Logger logger = LogManager.getLogger(RequestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(RequestController.class);
 
     @GetMapping
     public String home(){
-        logger.info("RequestController home");
+        logger.trace("RequestController home");
         return "home";
     }
 
     @GetMapping("/welcome/{name}")
     public String welcome(@PathVariable String name){
-        logger.info("RequestController welcome");
+        logger.trace("RequestController welcome");
         return "welcome " + name ;
     }
 
     @GetMapping("/process")
     public String process() throws InterruptedException {
-        logger.info("RequestController process");
+        logger.trace("RequestController process");
         Thread.sleep(5000);
         logger.info("RequestController process completed");
 
