fc4c5b97725c9b02397ed12a209dc19531df6a7d
flaviano-faria <flsilv89@yahoo.com.br>
2025-12-26 21:32:29 -0300

    Configuring client http and implementing controller

--- src/main/java/com/springotel/controller/RequestController.java
+++ src/main/java/com/springotel/controller/RequestController.java
@@ -1,9 +1,36 @@
 package com.springotel.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
+
+    private static final Logger logger = LogManager.getLogger(RequestController.class);
+
+    @GetMapping
+    public String home(){
+        logger.info("RequestController home");
+        return "home";
+    }
+
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable String name){
+        logger.info("RequestController welcome");
+        return "welcome " + name ;
+    }
+
+    @GetMapping("/process")
+    public String process() throws InterruptedException {
+        logger.info("RequestController process");
+        Thread.sleep(5000);
+        logger.info("RequestController process completed");
+
+        return "processed";
+    }
 
 
 }
